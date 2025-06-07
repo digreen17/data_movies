85ffdfd21624965b4f12a6aa0f6af65a74f822c7
Daria <digreen17@mail.ru>
2025-06-07 15:53:59 +0300

    feat: change script process_data

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -1,6 +1,8 @@
 import argparse
 from pathlib import Path
+from datetime import datetime
 import pandas as pd
+
 
 def read_raw_tmdb(path: Path) -> pd.DataFrame:
     return pd.read_csv(
@@ -20,6 +22,7 @@ def read_raw_tmdb(path: Path) -> pd.DataFrame:
         parse_dates=["release_date"],
     )
 
+
 def read_raw_cpi(path: Path) -> pd.DataFrame:
     return pd.read_csv(
         path,
@@ -27,50 +30,46 @@ def read_raw_cpi(path: Path) -> pd.DataFrame:
     ).rename(columns={"year": "cpi_date"})
 
 
-def process_data(df_tmdb: pd.DataFrame, df_cpi: pd.DataFrame) -> pd.DataFrame:
-    
-    latest_cpi = df_cpi.loc[df_cpi["cpi_date"].idxmax(), "cpi"]
-
+def process_tmdb(df_tmdb: pd.DataFrame) -> pd.DataFrame:
     str_column = df_tmdb.select_dtypes(include="object").columns
     for col in str_column:
         df_tmdb[col] = df_tmdb[col].str.lower().str.strip()
 
-    df_tmdb["release_month"] = (
-        df_tmdb["release_date"].dt.to_period("M").dt.to_timestamp()
+    df_tmdb["release_month"] = (df_tmdb["release_date"].dt.to_period("M").dt.to_timestamp())
+    df_tmdb["main_country"] = df_tmdb["production_countries"].str.split(",", expand=True)[0]
+
+    df_tmdb["genres"] = df_tmdb["genres"].fillna("undefined")
+
+    return df_tmdb
+
+
+def filter_tmdb(df_tmdb: pd.DataFrame, min_release_year: datetime, min_q: float=0.3, max_q: float=0.99) -> pd.DataFrame:
+    
+    df_tmdb = df_tmdb.drop_duplicates(subset=["original_title", "release_date"])
+    df_tmdb = df_tmdb.loc[df_tmdb["release_date"] > min_release_year]
+    filter_not_zero = df_tmdb["revenue"] > 0
+    quantile_min = df_tmdb.loc[filter_not_zero, "revenue"].quantile(min_q)
+    quantile_max = df_tmdb.loc[filter_not_zero, "revenue"].quantile(max_q)
+
+    df_filtered = df_tmdb.loc[(df_tmdb["revenue"] > quantile_min) & (df_tmdb["revenue"] < quantile_max)]
+
+    return df_filtered
+
+
+def process_data(df_tmdb: pd.DataFrame, df_cpi: pd.DataFrame) -> pd.DataFrame:
+    df_tmdb = process_tmdb(df_tmdb)
+    df_tmdb_filtered = filter_tmdb(df_tmdb, df_cpi["cpi_date"].min())
+
+    df_merged = df_tmdb_filtered.merge(df_cpi, left_on="release_month", right_on="cpi_date", how="left")
+
+    latest_cpi = df_cpi.loc[df_cpi["cpi_date"].idxmax(), "cpi"]
+    df_merged["current_budget"] = df_merged["budget"] * (latest_cpi / df_merged["cpi"])
+
+    df_merged["current_revenue"] = df_merged["revenue"] * (
+        latest_cpi / df_merged["cpi"]
     )
-
-    df_merged = df_tmdb.merge(
-        df_cpi, left_on="release_month", right_on="cpi_date", how="left"
-    )
-
-    df_merged = df_merged.drop_duplicates(subset=["original_title", "release_date"])
-
-    df_merged = df_merged[df_merged["cpi"].notna()].reset_index(drop=True)
-
-    filter_not_zero = df_merged["revenue"] > 0
-    quantile_min = df_merged.loc[filter_not_zero, "revenue"].quantile(0.3)
-    quantile_max = df_merged.loc[filter_not_zero, "revenue"].quantile(0.99)
 
     df_filtered = df_merged[
-        (df_merged["revenue"] > quantile_min) & (df_merged["revenue"] < quantile_max)
-    ]
-    df_filtered = df_filtered.copy()
-
-    df_filtered["main_country"] = df_filtered["production_countries"].str.split(
-        ",", expand=True
-    )[0]
-
-    df_filtered["genres"] = df_filtered["genres"].fillna("undefined")
-
-    df_filtered["current_budget"] = df_filtered["budget"] * (
-        latest_cpi / df_filtered["cpi"]
-    )
-
-    df_filtered["current_revenue"] = df_filtered["revenue"] * (
-        latest_cpi / df_filtered["cpi"]
-    )
-
-    df_filtered = df_filtered[
         [
             "id",
             "title",
@@ -86,10 +85,9 @@ def process_data(df_tmdb: pd.DataFrame, df_cpi: pd.DataFrame) -> pd.DataFrame:
             "current_budget",
             "current_revenue",
         ]
-    ].copy()
+    ]
     df_filtered = df_filtered.reset_index(drop=True)
-    return df_filtered 
-
+    return df_filtered
 
 
 if __name__ == "__main__":
